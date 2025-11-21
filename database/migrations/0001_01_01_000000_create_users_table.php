becb18bc78a5478ac3612088829cdeb555ded19b
Atha Adnan <athaadnanreswara191@gmail.com>
2025-11-21 14:16:28 +0700

    tempeteeee

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->role('role',['admin','developer','penguji']);
+            $table->enum('role',['admin','developer']);
             $table->rememberToken();
             $table->timestamps();
         });
